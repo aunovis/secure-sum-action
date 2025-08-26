27b7344990bfecef7c9ba9e56854e426d148513b
Simon Heidrich <Heidrich@aunovis.de>
2025-08-26 07:27:15 +0200

    Updating examples.

--- scripts/update_examples.sh
+++ scripts/update_examples.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-cd "$(git rev-parse --show-toplevel)"
+git_root="$(git rev-parse --show-toplevel)"
+exmaple_dir="$git_root/examples"
 
-curl https://raw.githubusercontent.com/aunovis/secure_sum/refs/heads/main/Cargo.toml -o example_cargo.toml
-curl https://raw.githubusercontent.com/aunovis/secure_sum/refs/heads/main/system_tests/example_metric.toml -o example_metric.toml
+cd $git_root
+
+if [ ! -d "$exmaple_dir" ]; then
+    mkdir "$exmaple_dir"
+fi
+
+curl https://raw.githubusercontent.com/aunovis/secure_sum/refs/heads/main/Cargo.toml -o "$exmaple_dir/example_cargo.toml"
+curl https://raw.githubusercontent.com/Weichwerke-Heidrich-Software/setup-bomnipotent-action/refs/heads/main/package.json -o "$exmaple_dir/example_package.json"
+curl https://raw.githubusercontent.com/aunovis/secure_sum/refs/heads/main/system_tests/example_metric.toml -o "$exmaple_dir/example_metric.toml"
