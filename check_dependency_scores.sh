5523c96b370fea29dfd409da319405e24fff0226
Simon Heidrich <Heidrich@aunovis.de>
2025-08-26 07:34:02 +0200

    Calling secure_sum with arbitrary arguments in script.

--- check_dependency_scores.sh
+++ check_dependency_scores.sh
@@ -2,6 +2,11 @@
 
 set -eou pipefail
 
+# This is a tiny wrapper script that ensures that secure_sum is installed, and passes any arguments on.
+# You can thus call it just as you would secure_sum.
+
+args=("$@")
+
 function get_latest_tag() {
     local repo=aunovis/secure_sum
     curl -s "https://api.github.com/repos/${repo}/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'
@@ -12,3 +17,5 @@ if ! command -v secure_sum > /dev/null; then
     echo "Installing AUNOVIS Secure Sum..."
     curl --proto '=https' --tlsv1.2 -LsSf "https://github.com/aunovis/secure_sum/releases/download/${version}/secure_sum-installer.sh" | sh
 fi
+
+secure_sum "${args[@]}"
