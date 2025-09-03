4431efe5ddce0503df28cc73425ee65a39fd4d8d
Simon Heidrich <Heidrich@aunovis.de>
2025-09-03 07:55:02 +0200

    Making installer more robust and more verbose.

--- check_dependency_scores.sh
+++ check_dependency_scores.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 set -eou pipefail
-set -x
 
 # This is a tiny wrapper script that ensures that secure_sum is installed, and passes any arguments on.
 # You can thus call it just as you would secure_sum.
 
 args=("$@")
 
-function get_latest_tag() {
-    curl -s "https://api.github.com/repos/aunovis/secure_sum/releases/latest" | grep '"tag_name":' | cut -d '"' -f4
-}
-
-if ! command -v secure_sum > /dev/null; then
-    version=$(get_latest_tag)
+function install_latest_version() {
+    local repo_metadata=$(curl -s "https://api.github.com/repos/aunovis/secure_sum/releases/latest")
+    local tag_line=$(echo "$repo_metadata" | grep '"tag_name":')
+    echo "Detected line:"
+    echo "$tag_line"
+    local version=$(echo "$tag_line" | cut -d '"' -f4)
+    if [ -z "$version" ]; then
+        echo "Could not parse version, trying jq."
+        if ! command -v jq; then
+            echo "jq is not installed, I ran out of ideas."
+            exit 1
+        fi
+        version=$(echo "$repo_metadata" | jq -r '.tag_name')
+    fi
     echo "Installing AUNOVIS Secure Sum ${version}..."
     installer_url="https://github.com/aunovis/secure_sum/releases/download/${version}/secure_sum-installer.sh"
     echo "Using installer from URL: ${installer_url}"
     curl --proto '=https' --tlsv1.2 -LsSf "$installer_url" | sh
+}
+
+if ! command -v secure_sum > /dev/null; then
+    install_latest_version
 fi
 
 secure_sum "${args[@]}"
