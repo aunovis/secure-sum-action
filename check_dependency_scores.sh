6cac0c97e5b90e224b3eb6f9867f1df4a4e9b247
Simon Heidrich <Heidrich@aunovis.de>
2025-09-03 07:59:16 +0200

    More robustness.

--- check_dependency_scores.sh
+++ check_dependency_scores.sh
@@ -8,21 +8,30 @@ set -eou pipefail
 args=("$@")
 
 function install_latest_version() {
-    local repo_metadata=$(curl -s "https://api.github.com/repos/aunovis/secure_sum/releases/latest")
+    if ! command -v curl > /dev/null; then
+        echo "Please install curl first."
+        exit 1
+    fi
+    local metadata_url="https://api.github.com/repos/aunovis/secure_sum/releases/latest"
+    local repo_metadata=$(curl -s "$metadata_url")
+    if [ -z "$repo_metadata" ]; then
+        echo "URL $metadata_url did not return any metadata."
+        exit 1
+    fi
     local tag_line=$(echo "$repo_metadata" | grep '"tag_name":')
     echo "Detected line:"
     echo "$tag_line"
     local version=$(echo "$tag_line" | cut -d '"' -f4)
     if [ -z "$version" ]; then
         echo "Could not parse version, trying jq."
-        if ! command -v jq; then
+        if ! command -v jq > /dev/null; then
             echo "jq is not installed, I ran out of ideas."
             exit 1
         fi
         version=$(echo "$repo_metadata" | jq -r '.tag_name')
     fi
     echo "Installing AUNOVIS Secure Sum ${version}..."
-    installer_url="https://github.com/aunovis/secure_sum/releases/download/${version}/secure_sum-installer.sh"
+    local installer_url="https://github.com/aunovis/secure_sum/releases/download/${version}/secure_sum-installer.sh"
     echo "Using installer from URL: ${installer_url}"
     curl --proto '=https' --tlsv1.2 -LsSf "$installer_url" | sh
 }
